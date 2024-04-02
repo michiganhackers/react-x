86f82ca627f565e869d8be712426f43cb945749e
Brian Travis <bztravis88@gmail.com>
2024-04-02 11:48:49 -0400

    Add Input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Input from './components/Input'
 import Post from './components/Post'
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
       <div className="nav">
         <img className="logo" alt="logo" src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1712016000&semt=ais" />
       </div>
+      <Input />
       <div className="postContainer">
         {posts.map((post) => <Post post={post} />)}
       </div>
---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+const Input = () => {
+  return (
+    <div className="inputContainer">
+      <input type="text" placeholder="What is happening!?" className="postInput" />
+      <button className="postButton">Post</button>
+    </div>
+  )
+}
+export default Input

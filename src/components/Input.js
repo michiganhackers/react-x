07e07c9be083e4c1ac053d6406293b0320c45756
Brian Travis <bztravis88@gmail.com>
2024-04-03 17:30:50 -0400

    Add Input text controlled component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,14 @@
+import { useState } from 'react'
+
+const USERNAME = 'briantravis'
+
 const Input = ({ setPosts }) => {
+
+  const [text, setText] = useState('')
 
   return (
     <div className="inputContainer">
-      <input type="text" placeholder="What is happening!?" className="postInput" />
+      <input type="text" placeholder="What is happening!?" className="postInput" value={text} onChange={(e) => setText(e.target.value)} />
       <button className="postButton">Post</button>
     </div>
   )
